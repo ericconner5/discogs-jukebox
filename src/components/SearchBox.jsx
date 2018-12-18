46a0843fcd8008a370584b90fb5a7e0286d12c14
Eric Conner <theluvq@gmail.com>
2018-12-18 10:53:18 -0800

    add handleNewSearch function, font file

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -4,14 +4,23 @@ import React from 'react';
 function SearchBox(){
   let _label;
 
+  function handleNewSearch(event) {
+    event.preventDefault();
+    console.log(_label.value);
+    _label.value = '';
+    return console.log("search sent")
+  }
+
   return (
     <div className="search">
       <p>find music by:</p>
       <p>Record Label</p>
       <div>
-        <input type="text" className="input-field" placeholder="Enter Record Label"
-          ref={(input) => {_label = input;}}></input>
-        <button type="submit"><i class="submit-search"></i>Search</button>
+        <form onSubmit={handleNewSearch}>
+          <input type="text" className="input-field" placeholder="Enter Record Label"
+            ref={(input) => {_label = input;}}></input>
+          <button type="submit">Search</button>
+        </form>
       </div>
     </div>
 
